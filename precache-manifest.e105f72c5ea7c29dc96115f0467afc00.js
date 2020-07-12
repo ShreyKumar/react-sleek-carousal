8362bd9435dc2566679db27b3221487d68d9ecb4
Shreyansh Kumar <shreyansh.kumar@hotmail.com>
2020-07-11 20:09:21 -0400

    Updates

---
+++ precache-manifest.e105f72c5ea7c29dc96115f0467afc00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "319a24aa48183742a911a120abbc10c4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "54ecbe3dc0daed805acf",
+    "url": "./static/css/main.c8b7cf3d.chunk.css"
+  },
+  {
+    "revision": "a76393c08d328c20907c",
+    "url": "./static/js/2.9b1d3387.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "./static/js/2.9b1d3387.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54ecbe3dc0daed805acf",
+    "url": "./static/js/main.de61dd2c.chunk.js"
+  },
+  {
+    "revision": "e8777407a787aee3c948",
+    "url": "./static/js/runtime-main.9a437406.js"
+  },
+  {
+    "revision": "d7d86e7e7e6fdfcac55037c13bafde3a",
+    "url": "./static/media/Galada-Regular.d7d86e7e.ttf"
+  }
+]);
